fef17184106ba89733ac591173731cd4a6506bce
Kerry Campbell <kerryfranciscampbell@icloud.com>
2026-09-07 09:08:12 -0400

    Fix RuboCop lint issues

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :project
 
-  STATUSES = ["Open", "In Progress", "Resolved"].freeze
-  PRIORITIES = ["Low", "Medium", "High", "Critical"].freeze
+  STATUSES = [ "Open", "In Progress", "Resolved" ].freeze
+  PRIORITIES = [ "Low", "Medium", "High", "Critical" ].freeze
 
   validates :title, presence: true
   validates :status, inclusion: { in: STATUSES }
